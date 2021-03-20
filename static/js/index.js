d40bca22f99ca141e9d765bac57db82b71acb12c
Jun Yoo <hjyoo125@gmail.com>
2021-03-20 16:51:23 -0700

    key control added

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,13 @@
-let minus = document.getElementById("minus");
-let plus = document.getElementById("plus");
-let reset = document.getElementById("reset");
-let number = document.getElementById("number");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const reset = document.getElementById("reset");
+const number = document.getElementById("number");
 
 plus.addEventListener("click", addUp);
 function addUp() {
   let currentNumber = number.innerHTML;
   number.innerHTML = Number(currentNumber) + 1;
 }
-plus.addEventListener("keydown", keys => {
-  if (keys === "a") {
-    addUp()
-  }
-});
 
 minus.addEventListener("click", subDown);
 function subDown() {
@@ -26,3 +21,13 @@ reset.addEventListener("click", resetIt);
 function resetIt() {
   number.innerHTML = 0;
 }
+
+document.addEventListener("keydown", e => {
+  if (e.key === "a" || e.key === "+") {
+    addUp();
+  } else if (e.key === "s" || e.key === "-") {
+    subDown();
+  } else if (e.key === "r" || e.key === "0") {
+    resetIt();
+  }
+});
