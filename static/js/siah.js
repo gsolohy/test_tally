ef513be267e771682fe4e33ee12fb471ea967ed5
Jun Yoo <hjyoo125@gmail.com>
2021-03-20 14:19:40 -0700

    initial commit

---
+++ static/js/siah.js
@@ -0,0 +1,23 @@
+let minus = document.getElementById("minus");
+let plus = document.getElementById("plus");
+let reset = document.getElementById("reset");
+let number = document.getElementById("number");
+
+plus.addEventListener("click", addUp);
+function addUp() {
+  let currentNumber = number.innerHTML;
+  number.innerHTML = Number(currentNumber) + 1;
+}
+
+minus.addEventListener("click", subDown);
+function subDown() {
+  let currentNumber = number.innerHTML;
+  if (currentNumber > 0) {
+    number.innerHTML = Number(currentNumber) - 1;
+  }
+}
+
+reset.addEventListener("click", resetIt);
+function resetIt() {
+  number.innerHTML = 0;
+}
